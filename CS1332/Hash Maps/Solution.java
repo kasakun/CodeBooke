c1dfe9906294133d5b8b91101ed1d65c08c46690
Zeyu Chen <chenzy@gatech.edu>
2018-06-21 20:40:36 -0700

    Solution of Hash Map added.

--- CS1332/Hash Maps/Solution.java
+++ CS1332/Hash Maps/Solution.java
@@ -4,6 +4,15 @@ public class Solution {
     public static void main(String args[]) {
         System.out.println("Test for Hash Map.");
         HashMap<Integer, Integer> map = new HashMap<>();
+        System.out.println("Put (3, 4)");
         map.put(3, 4);
+        System.out.println("Map contains 3?  :" + map.containsKey(3));
+        System.out.print("Map contains 12? :" + map.containsKey(12));
+        System.out.println(" size: " + map.size());
+
+        System.out.println("Put (12, 34)");
+        map.put(12, 34);
+        System.out.print("Map contains 12? :" + map.containsKey(12));
+        System.out.println(" size: " + map.size());
     }
 }
