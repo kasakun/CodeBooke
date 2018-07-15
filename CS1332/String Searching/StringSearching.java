72ddcb4dc2f475c48fa5fc80befd04969799d4f5
Zeyu Chen <chenzy@gatech.edu>
2018-07-14 23:50:43 -0700

    Rabin-Karp added

--- CS1332/String Searching/StringSearching.java
+++ CS1332/String Searching/StringSearching.java
@@ -187,4 +187,186 @@ public class StringSearching {
         }
         return lastTable;
     }
+
+    /**
+     * Prime base used for Rabin-Karp hashing.
+     * DO NOT EDIT!
+     */
+    private static final int BASE = 181;
+
+    /**
+     * Runs Rabin-Karp algorithm. Generate the pattern hash, and compare it with
+     * the hash from a substring of text that's the same length as the pattern.
+     * If the two hashes match, compare their individual characters, else update
+     * the text hash and continue.
+     *
+     * @throws IllegalArgumentException if the pattern is null or of length 0
+     * @throws IllegalArgumentException if text or comparator is null
+     * @param pattern a string you're searching for in a body of text
+     * @param text the body of text where you search for pattern
+     * @param comparator the comparator to use when checking character equality
+     * @return list containing the starting index for each match found
+     */
+    public static List<Integer> rabinKarp(CharSequence pattern,
+                      CharSequence text, CharacterComparator comparator) {
+        if (pattern  == null || pattern.length() == 0) {
+            throw new IllegalArgumentException("Null or empty pattern cannot"
+                    + " be used in string searching");
+        }
+
+        if (text == null || comparator == null) {
+            throw new IllegalArgumentException("Null text or "
+                    + "null comparator cannot be used in string searching");
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        int patternHash = generateHash(pattern, pattern.length());
+        int textHash = generateHash(text, pattern.length());
+
+        int i = 0;
+        while (i <= text.length() - pattern.length()) {
+            if (patternHash == textHash) {
+
+                int j = 0;
+                while (j < pattern.length()
+                        && comparator.compare(text.charAt(i + j),
+                        pattern.charAt(j)) == 0) {
+                    j++;
+                }
+                if (j == pattern.length()) {
+                    result.add(i);
+                }
+
+            }
+
+            int len = pattern.length();
+
+            if (i + len < text.length()) {
+                textHash = updateHash(textHash, len, text.charAt(i),
+                        text.charAt(i + len));
+            }
+            ++i;
+        }
+
+        return result;
+    }
+
+    /**
+     * Hash function used for Rabin-Karp. The formula for hashing a string is:
+     *
+     * sum of: c * BASE ^ (pattern.length - 1 - i), where c is the integer
+     * value of the current character, and i is the index of the character
+     *
+     * For example: Hashing "bunn" as a substring of "bunny" with base 181 hash
+     * = b * 181 ^ 3 + u * 181 ^ 2 + n * 181 ^ 1 + n * 181 ^ 0 = 98 * 181 ^ 3 +
+     * 117 * 181 ^ 2 + 110 * 181 ^ 1 + 110 * 181 ^ 0 = 584967675
+     *
+     * Note that the largest possible value for an int is 2147483647, so it is
+     * possible that the calculated hash may roll over.
+     *
+     *
+     * Do NOT use {@code Math.pow()} in this method. We have provided a pow()
+     * method for you to use.
+     *
+     * @throws IllegalArgumentException if current is null
+     * @throws IllegalArgumentException if length is negative, 0, or greater
+     *     than the length of current
+     * @param current substring you are generating hash function for
+     * @param length the length of the string you want to generate the hash for,
+     * starting from index 0. For example, if length is 4 but current's length
+     * is 6, then you include indices 0-3 in your hash (and pretend the actual
+     * length is 4)
+     * @return hash of the substring
+     */
+    public static int generateHash(CharSequence current, int length) {
+        if (current == null) {
+            throw new IllegalArgumentException("Cannot generate hash from"
+                    + "null string");
+        }
+
+        if (length <= 0 || length > current.length()) {
+            throw new IllegalArgumentException("Invalid length, length is"
+                    + "less than or equal to zero or greater than the length"
+                    + "of current string");
+        }
+
+        int hash = 0;
+        for (int i = 0; i < length; i++) {
+            hash += current.charAt(i) * pow(BASE, length - 1 - i);
+        }
+        return hash;
+    }
+
+    /**
+     * Updates a hash in constant time to avoid constantly recalculating
+     * entire hash. To update the hash:
+     *
+     *  remove the oldChar times BASE raised to the length - 1, multiply by
+     *  BASE, and add the newChar.
+     *
+     * For example: Shifting from "bunn" to "unny" in "bunny" with base 181
+     * hash("unny") = (hash("bunn") - b * 181 ^ 3) * 181 + y * 181 ^ 0 =
+     * (584967675 - 98 * 181 ^ 3) * 181 + 121 * 181 ^ 0 = 697403438
+     *
+     *
+     * The computation of BASE raised to length - 1 may require O(log n) time,
+     * but the method should otherwise run in O(1).
+     *
+     * Do NOT use {@code Math.pow()} in this method. We have provided a pow()
+     * method for you to use.
+     *
+     * @throws IllegalArgumentException if length is negative or 0
+     * @param oldHash hash generated by generateHash
+     * @param length length of pattern/substring of text
+     * @param oldChar character we want to remove from hashed substring
+     * @param newChar character we want to add to hashed substring
+     * @return updated hash of this substring
+     */
+    public static int updateHash(int oldHash, int length, char oldChar,
+                                 char newChar) {
+        if (length <= 0) {
+            throw  new IllegalArgumentException("length cannot be zero"
+                    + "or negative");
+        }
+
+        int newHash;
+
+        oldHash = oldHash - oldChar*pow(BASE, length - 1);
+        oldHash = oldHash*BASE;
+        newHash = oldHash + newChar;
+
+        return newHash;
+    }
+
+    /**
+     * Calculate the result of a number raised to a power.
+     *
+     * DO NOT MODIFY THIS METHOD.
+     *
+     * @throws IllegalArgumentException if both {@code base} and {@code exp} are
+     * 0
+     * @throws IllegalArgumentException if {@code exp} is negative
+     * @param base base of the number
+     * @param exp power to raise the base to. Must be 0 or greater.
+     * @return result of the base raised to that power
+     */
+    private static int pow(int base, int exp) {
+        if (exp < 0) {
+            throw new IllegalArgumentException("Exponent cannot be negative.");
+        } else if (base == 0 && exp == 0) {
+            throw new IllegalArgumentException(
+                    "Both base and exponent cannot be 0.");
+        } else if (exp == 0) {
+            return 1;
+        } else if (exp == 1) {
+            return base;
+        }
+        int halfPow = pow(base, exp / 2);
+        if (exp % 2 == 0) {
+            return halfPow * halfPow;
+        } else {
+            return halfPow * pow(base, (exp / 2) + 1);
+        }
+    }
 }
