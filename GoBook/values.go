eb7b1a3834a776ca860cdf3254015b0b7677924d
Zeyu Chen <chenzy@gatech.edu>
2018-07-21 22:10:59 -0700

    values added.

---
+++ GoBook/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "hello")
+
+	fmt.Println("23+43 =", 23+34)
+	fmt.Println("4.5/0.9", 4.5/0.9)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
