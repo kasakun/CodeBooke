132c10ad46025fcbdeb5300067c3ef2df7051f6e
Zeyu Chen <chenzy@gatech.edu>
2018-08-14 23:45:58 -0700

    delete comments

--- GoBook/arrays.go
+++ GoBook/arrays.go
@@ -11,17 +11,11 @@ func main() {
 	fmt.Println("set:", a)
 	fmt.Println("get:", a[4])
 
-	// The builtin `len` returns the length of an array.
 	fmt.Println("len:", len(a))
 
-	// Use this syntax to declare and initialize an array
-	// in one line.
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
-	// Array types are one-dimensional, but you can
-	// compose types to build multi-dimensional data
-	// structures.
 	var twoD [2][3]int
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 3; j++ {
