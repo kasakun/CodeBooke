826b25b95cfc9261397a46813970f37b724d10da
Zeyu Chen <chenzy@gatech.edu>
2019-01-14 02:49:52 -0800

    Binary Search method added.

--- LeetCode/300-Longest-Increasing-Subsequence(Medium)/Solution1.java
+++ LeetCode/300-Longest-Increasing-Subsequence(Medium)/Solution1.java
@@ -2,7 +2,7 @@
  * Dp
  * O(logn^2)
  */
-class Solution {
+class Solution1 {
     public int lengthOfLIS(int[] nums) {
         if (nums.length == 0)
             return 0;
--- LeetCode/300-Longest-Increasing-Subsequence(Medium)/Solution2.java
+++ LeetCode/300-Longest-Increasing-Subsequence(Medium)/Solution2.java
@@ -1,4 +1,32 @@
 /**
- * DP + BS
- * O(logn)
+ * arr + BS
+ * O(nlogn)
+ *
+ * 1. Create an array arr.
+ * 2. Insert an element in arr when traversing the nums.
+ * 3. If the num is not in the arr, which means the num is larger than
+ *    any element in arr, then ++len, otherwise replace the related element
+ *    in arr with current element which the len maintains the same.
+ * 4. At last, the len will be the longest length of the increasing subsequence.
  */
+Solution2 {
+    public int lengthOfLIS(int[] nums) {
+        int arr[] = new int[nums.length];
+        int len = 0;
+
+        for (int i = 0; i < nums.lenght; ++i) {
+            int j = Arrays.binarySearch(arr, 0, len, nums[i]);
+
+            if(j < 0) {
+                j = -(j + 1);
+            }
+
+            arr[j] = nums[i];
+            if (j == len) {
+                ++len;
+            }
+
+            return len;
+        }
+    }
+}
