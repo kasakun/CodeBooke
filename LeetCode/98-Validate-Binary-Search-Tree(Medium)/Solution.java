37acc460bf1b2a4afa0af5204ace45f1267c37e4
Zeyu Chen <chenzy@gatech.edu>
2019-02-10 04:25:54 -0800

    validate binary search tree added.

---
+++ LeetCode/98-Validate-Binary-Search-Tree(Medium)/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBSTHelper(TreeNode node, long lower, long upper) {
+        if (node == null)
+            return true;
+        
+        if (node.val <= lower || node.val >= upper)
+            return false;
+        
+        return isValidBSTHelper(node.left, lower, node.val) &&
+               isValidBSTHelper(node.right, node.val, upper);
+    }
+}
