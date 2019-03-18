5ea027e56422cffe6169bb606b099172a418b7ff
Zeyu Chen <chenzy@gatech.edu>
2019-03-18 02:30:31 -0700

    integer break added.

---
+++ LeetCode/343-Integer-Break(Medium)/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Math trick
+ * 
+ * perfect factor: 3, 2
+ * 3*3>2*2*2
+ */
+class Solution {
+    public int integerBreak(int n) {
+        if (n == 3) return 2;
+        if (n == 2) return 1;
+        
+        int res = 1;
+        
+        while (n > 4) {
+            res *= 3;
+            n -= 3;
+        }
+        
+        res *= n;
+        
+        return res;
+    }
+}
