bff912bfe1575088022df7a7ce2529d59217d59b
Zeyu Chen <chenzy@gatech.edu>
2019-05-18 10:14:23 -0700

    solution added.

---
+++ LeetCode/162-Find-Peak-Element(Medium)/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1) return 0;
+        
+        int prev = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; ++i) {
+            if (prev > nums[i]) return (i - 1);
+            
+            prev = nums[i];
+        }
+        
+        return nums.length - 1;
+    }
+}
