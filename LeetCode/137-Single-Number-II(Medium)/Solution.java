a518e19493e7144e796a63fd803bddb9b4af54f3
Zeyu Chen <chenzy@gatech.edu>
2019-05-19 00:25:16 -0700

    change to a more reasonable solution.

--- LeetCode/137-Single-Number-II(Medium)/Solution.java
+++ LeetCode/137-Single-Number-II(Medium)/Solution.java
@@ -1,10 +1,19 @@
+/**
+ * when 1 comes jump like:
+ * 00->01->10->00
+ * when 0 comes, hold
+ *
+ * we can deduct logic expression
+ */
 class Solution {
     public int singleNumber(int[] nums) {
         int a = 0, b = 0;
         
         for (int num : nums) {
-            b = b ^ num & ~a;
-            a = a ^ num & ~b;
+            int prev_a = a, prev_b = b;
+
+            a = (~num & prev_a & ~prev_b)|(num & ~prev_a & prev_b);
+            b = (~num & ~prev_a & prev_b)|(num & ~prev_a & ~prev_b);
         }
         
         return b;
